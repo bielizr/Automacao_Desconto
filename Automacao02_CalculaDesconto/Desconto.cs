e3d77c83605afafe70d5da0a58a89d2d3db71d90
etec <etec@L06M08>
2025-05-12 20:29:30 -0300

    Adicionar arquivos de projeto.

---
+++ Automacao02_CalculaDesconto/Desconto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automacao02_CalculaDesconto
+{
+    public class Desconto
+    {
+        // Define de vai ou não ter desconto (valores >= 350 têm desconto
+        public bool recebeDesconto(float valor)
+        {
+            return (valor >= 350);
+        }
+
+        /* Calculo o % de desconto a ser aplicado (se houver)
+          *Valor maior ou igual a R$ 5.000,00 ---> 3,0% desconto
+          *Valor maior ou igual a R$ 3.000,00 ---> 2,0% desconto
+          *Valor maior ou igual a R$ 1.000,00 ---> 1,0% desconto
+          *Valor maior ou igual a R$   350,00 ---> 0,5% desconto */
+        public float percentualDesconto(float valor)
+        {
+            if (valor >= 5000) return 0.03f;
+            if (valor >= 3000) return 0.02f;
+            if (valor >= 1000) return 0.01f;
+            if (valor >= 350) return 0.005f;
+            return 0f;
+
+        }
+
+        /* Calcular o valor do desconto (se houver) */
+        public float valorDesconto(float valor)
+        {
+            return valor * percentualDesconto(valor);
+        }
+
+        /* Calcular valor final (considerando desconto, se houver) */
+        public float totalCompra(float valor)
+        {
+            return valor - valorDesconto(valor);
+        }
+    }
+}
---
+++ Automacao02_CalculoDescontoTest/DescontoUnitTest.cs
@@ -0,0 +1,50 @@
+﻿using Automacao02_CalculaDesconto;
+
+namespace Automacao02_CalculoDescontoTest
+{
+    [TestClass]
+    public sealed class DescontoUnitTest
+    {
+        private Desconto desconto;
+
+        [TestInitialize]
+        public void setup()
+        {
+            desconto = new Desconto();
+        }
+
+        [TestMethod]
+        [DataRow(200, false)]
+        [DataRow(420, true)]
+        [DataRow(1200, true)]
+        [DataRow(3725, true)]
+        [DataRow(5800, true)]
+        [DataRow(350, true)]
+        [DataRow(1000, true)]
+        [DataRow(3000, true)]
+
+        public void valor200_DeveVerificarDesconto(float valor, bool esperado)
+        {
+            // Arange - Prepara tudo       
+            // Act - Executa o artefato a ser testado
+            bool obtido = desconto.recebeDesconto(valor);
+
+            // Assert - Compara resultados e decide se passou
+            Assert.AreEqual(esperado, obtido);
+            //Assert.IsTrue(obtido==esperado);
+
+
+        }
+
+        [TestMethod]
+        [DataRow(200, 0)]
+        public void valor_DeveRetornarPercDesconto(float valor, float esperado)
+        {
+            //Act - Executa o artefato a ser testado
+            float obtido = desconto.percentualDesconto(valor);
+
+            //Assert - Comparando resultados e decidindo se aprovado
+            Assert.AreEqual(esperado, obtido);
+        }
+    }
+}
